f42276a5e87b0f216c79b04bb307ebb056d1e795
ThreesshiNaidu <threesshinaidu@gmail.com>
2022-01-05 10:58:37 +0000

    yeet was sooo close

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+    let Name = ""
+    let Age = ""
+
+    async function getAge() {
+  fetch('https://api.agify.io/?name=' + Name)
+	.then((response) => response.json())
+	.then((data) => { // Path for temperature
+  	Age = data.age; // Path for description
+	});
+}
+</script>
+
+<h1>Guess my Age App</h1>
+<p>Age:{Age}</p>
+
+<form on:submit|preventDefault={getAge}>
+    <label for="name">Name : </label>
+    <input id="name" bind:value={Name} type="text" />
+    <button type="name">Check</button>
+</form>
